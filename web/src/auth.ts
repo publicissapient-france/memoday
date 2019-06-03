ac9201b1701594a169ff3c765f13b4f465ad51ce
Benjamin Lacroix <blacroix@xebia.fr>
2019-06-03 15:22:50 +0200

    feat(offline): enable offline firestore mode

--- web/src/auth.ts
+++ web/src/auth.ts
@@ -1,17 +1,6 @@
 import firebase from 'firebase/app';
 import firebaseui from 'firebaseui';
-import {UserModule} from '@/store/user';
-
-const config = {
-  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-};
-
-firebase.initializeApp(config);
+import { UserModule } from '@/store/user';
 
 class Auth {
   public context: any;
